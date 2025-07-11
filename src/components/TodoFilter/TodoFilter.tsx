e0cb45dd86ff51e11ec7a472d75288e9b2182ca4
Oleksandr <lowka2018@gmail.com>
2025-07-11 23:17:59 +0200

    add react dynamic list of todos 3

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -46,7 +46,7 @@ export const TodoFilter: React.FC<Props> = ({
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        {query && (
+        {(query !== '' || statusFilter !== 'all') && (
           <span className="icon is-right" style={{ pointerEvents: 'all' }}>
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
             <button
